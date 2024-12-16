8ad631307774c2c8adf66992c0c98b803a2b68aa
James Hobin <hobinjk@gmail.com>
2024-12-16 13:05:23 -0500

    Add floating error window

--- init.lua
+++ init.lua
@@ -922,3 +922,10 @@ require('lazy').setup({
 
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
+
+vim.api.nvim_create_autocmd({ 'CursorHold', 'CursorHoldI' }, {
+  group = vim.api.nvim_create_augroup('float_diagnostic', { clear = true }),
+  callback = function()
+    vim.diagnostic.open_float(nil, { focus = false })
+  end,
+})
